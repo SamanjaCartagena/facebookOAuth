80494c2f6e439c97dfc0675a8675fa094d4f4c75
Samanja Cartagena <70960407+SamanjaCartagena@users.noreply.github.com>
2021-03-26 01:30:55 -0400

    facebook oauth with react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Facebook from './components/Facebook';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Facebook Auth Example</h1>
+      <p className="App-intro">
+        To get started authenticate with Facebook.
+      </p>
+      <Facebook/>
     </div>
   );
 }
